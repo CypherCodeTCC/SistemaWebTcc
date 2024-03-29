beba6e5ec42dd72367b6b2ccca11cc3b227581d4
FalecidoGugu <gutigeovany@gmail.com>
2024-03-29 03:04:18 -0300

    🎉 Adicionado Notificação no Carrinho

--- src/Components/NavBar/NavBar-components/CartLoginIcons.jsx
+++ src/Components/NavBar/NavBar-components/CartLoginIcons.jsx
@@ -1,14 +1,47 @@
-// CartLoginIcons.jsx
-import React from "react";
+import React, { useState } from "react";
 import CartIcon from "../../../Images/Svg/carrinho.svg";
 import SearchIcon from "../../../Images/Svg/search.svg";
 
+function PopupCarrinhoVazio({ setIsPopupOpen }) {
+    const handleFechar = () => {
+        setIsPopupOpen(false);
+        setMainHeadingZIndexBasedOnState(false);
+    };
+
+    return (
+        <div class="modal-overlay" onClick={handleFechar}>
+            <div class="modal">
+                <div class="modal-content">
+                    <i class="material-icons error-icon">error</i>
+                    <p>Carrinho vazio</p>
+                </div>
+            </div>
+        </div>
+
+    );
+}
+
+function setMainHeadingZIndexBasedOnState(isMenuOpen) {
+    const mainHeadings = document.querySelectorAll('.mainHeading');
+    mainHeadings.forEach(mainHeading => {
+        mainHeading.style.zIndex = isMenuOpen ? -1 : 1;
+    });
+}
+
 function CartLoginIcons() {
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+    const handleCartIconClick = () => {
+        setIsPopupOpen(true);
+        setMainHeadingZIndexBasedOnState(true);
+    };
+
     return (
         <div className="carrinho-logar desktop">
+            {isPopupOpen && <PopupCarrinhoVazio setIsPopupOpen={setIsPopupOpen} />}
             <div className="icones">
                 <img src={SearchIcon} alt="Ícone de busca" />
-                <img src={CartIcon} alt="Ícone de Carrinho" />
+                <img src={CartIcon} alt="Ícone de Carrinho" onClick={handleCartIconClick} />
             </div>
         </div>
     );
