13d55c8a93adfb15a830a72fedd382cd6a3205e7
Juan <juanpfreire18@gmail.com>
2024-04-19 10:36:05 -0300

    Criar rotas para o sistema.

---
+++ src/RoutesConfig.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Navbar from "./componentes/nav/NavBar.jsx";
+import Home from "./routes/Home.jsx";
+import Error from "./routes/error/Error.jsx";
+import Footer from "./componentes/footer/FooterBar.jsx";
+
+const Layout = ({children}) => (
+    <div>
+        <Navbar/>
+        <div>{children}</div>
+        <Footer/>
+    </div>
+)
+
+export default function RoutesConfig(){
+    return(
+        <BrowserRouter>      
+                <Routes>
+                    <Route path="/" element={<Layout><Home/></Layout>}/>
+                    <Route path="*" element={<Error/>}/>
+                </Routes>
+        </BrowserRouter>
+    );
+}
