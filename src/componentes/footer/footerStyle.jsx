62b7a659ecc52a26088bc0c9ed17028e7e5209ce
Juan <juanpfreire18@gmail.com>
2024-04-22 09:57:59 -0300

    Correções na estilização do Footer.

--- src/componentes/footer/footerStyle.jsx
+++ src/componentes/footer/footerStyle.jsx
@@ -1,65 +1,62 @@
 import styled from "styled-components";
 
 export const Footer = styled.section`
-  background-color: black;
-  color: white;
-  display: flex;
-  padding: 60px;
-  gap: 60px;
-  justify-content: center;
-
-  font-family: Arial, Helvetica, sans-serif;
-
-  flex-wrap: wrap;
-
-  .container-aboutUs,
-  .container-client,
-  .container-social-media,
-  .container-know-more,
-  .container-payment {
-    width: 200px;
-  }
-
-  .client {
+    background-color: black;
+    color: white;
     display: flex;
+    padding: 60px;
+    gap: 40px;
+    justify-content: center;
 
-    p {
-      padding-left: 10px;
+    font-family: Arial, Helvetica, sans-serif;
+
+    flex-wrap: wrap;
+
+    .container-aboutUs, .container-client, .container-social-media, .container-know-more, .container-payment{
+        width: 17%;
     }
+
+    .container-rights-reserved{
+        width: 100%;
+        text-align: center;
+    }
+
+    .client{
+        display: flex;
+    
     img {
       width: 35px;
       padding-top: 10px;
     }
-  }
+        
+    p {
+      padding-left: 10px;
+    }
+    }
 
-  .container-rights-reserved {
-    width: 100%;
+    .logo-image{
+        width: 100px;
+    }
+
+    button{
+        background-color: transparent;
+        padding-top: 15px;
+        text-decoration: underline;
+        color: gray;
+    }
+
+    p{
+        padding-top: 15px;
+        color: gray;
+    }
+
+    button:hover{
+        cursor:pointer;
+        transform: scale(1.1);
+        transition: 0.5s;
+    }
+
+@media (max-width: 1300px){
     text-align: center;
-  }
-
-  .logo-image {
-    width: 100px;
-  }
-
-  button {
-    background-color: transparent;
-    padding-top: 15px;
-    text-decoration: underline;
-    color: gray;
-  }
-
-  p {
-    padding-top: 15px;
-    color: gray;
-  }
-
-  button:hover {
-    cursor: pointer;
-    transform: scale(1.1);
-    transition: 0.5s;
-  }
-
-  @media (max-width: 1300px) {
-    text-align: center;
-  }
+}
 `;
