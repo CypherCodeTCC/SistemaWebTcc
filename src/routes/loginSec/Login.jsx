e52f4b08a7fa909d3016013a42b8f4deb1155ef0
Juan <juanpfreire18@gmail.com>
2024-04-29 21:37:20 -0300

    Criando componente Login.

---
+++ src/routes/loginSec/Login.jsx
@@ -0,0 +1,28 @@
+import PngCelular from '../../../public/celular.png'
+import { Container } from './loginStyle';
+
+export default function Login(){
+    return(
+        <>
+            <Container>
+                <div className='container-image'>
+                    <img src={PngCelular} alt="Imagem de Celular" />
+                </div>
+                <div className='container-form'>
+                    <h3>Seja Bem-Vindo</h3>
+                    <p>Faça login na sua conta Liber</p>
+                    <button>Prossiga com a Apple</button>
+                    <button>Prossiga com o Google</button>
+                    <button>Prossiga com o Facebook</button>
+                    <p className='center'>Ou</p>
+                    <h4>E-mail</h4>
+                    <input type="text" placeholder='Ex: meuemail@endereco.com'/>
+                    <h4>Senha</h4>
+                    <input type="text" placeholder='Insira sua palavra-passe' />
+                    <button className='btn-login'>Login</button>
+                    <p className='center'>Não tem uma conta? <span>Crie sua conta</span></p>
+                </div>
+            </Container>
+        </>
+    );
+}
