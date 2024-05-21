4742cf031ed4e818c25572f782d9424f86d60101
Juan <juanpfreire18@gmail.com>
2024-05-21 00:39:59 -0300

    [feat] bringing the books registered in the database to the section

--- src/routes/bestSellersSec/BestSellers.jsx
+++ src/routes/bestSellersSec/BestSellers.jsx
@@ -1,60 +1,78 @@
-import { 
+import {
     CardContainer,
     Imagem,
     Avaliacoes as AvaliacoesContainer,
-    NomeLivro,
-    NomeAutor,
-    PrecoLivro,
     Container,
     Titulo,
-    Fileira
-} from './bestSellers.style'
-
-//Estrelas do ReactIcons
-import { FaStar } from 'react-icons/fa';
-
-function Avaliacoes({ avaliacao }) {
+    Fileira,
+    NomeLivro,
+    PrecoLivro,
+  
+  } from "./bestSellers.style";
+  
+  //lib para enviar protocolos http para o servidor
+  import axios from "axios";
+  import { useEffect, useState } from "react";
+  
+  //Estrelas do ReactIcons
+  import { FaStar } from "react-icons/fa";
+  
+  function Avaliacoes({ avaliacao }) {
     const estrelas = [];
-
+  
     for (let i = 0; i < 5; i++) {
-        if (i < avaliacao) {
-            estrelas.push(<FaStar key={i} color="black" />);
-        } else {
-            estrelas.push(<FaStar key={i} color="lightgray" />);
-        }
+      if (i < avaliacao) {
+        estrelas.push(<FaStar key={i} color="black" />);
+      } else {
+        estrelas.push(<FaStar key={i} color="lightgray" />);
+      }
     }
-
+  
     return <AvaliacoesContainer>{estrelas}</AvaliacoesContainer>;
-}
-
-function Card({imagem, avaliacoes, nomeLivro, nomeAutor, precoLivro}) {
+  }
+  
+  function Card({ imagem, nome, preco, avaliacoes }) {
     return (
-        <CardContainer>
-            <Imagem src={imagem}></Imagem>
-            <Avaliacoes avaliacao={avaliacoes} />
-            <NomeLivro>{nomeLivro}</NomeLivro>
-            <NomeAutor>{nomeAutor}</NomeAutor>
-            <PrecoLivro>{precoLivro}</PrecoLivro>
-        </CardContainer>
+      <CardContainer>
+        <Imagem src={imagem}></Imagem>
+        <NomeLivro>{nome}</NomeLivro>
+        <Avaliacoes avaliacao={avaliacoes} />
+        <PrecoLivro>R${preco}</PrecoLivro>
+      </CardContainer>
     );
-}
-
-export default function BestSellers () {
+  }
+  
+  export default function BestSellers() {
+    const [books, setBooks] = useState([]);
+  
+    useEffect(() => {
+      const fetchAllBooks = async () => {
+        try {
+          const res = await axios.get("http://localhost:8081/books");
+          setBooks(res.data);
+        } catch (err) {
+          console.log(err);
+        }
+      };
+      fetchAllBooks();
+    }, []);
+  
+    console.log(books);
+  
     return (
-        <Container>
-            <Titulo>Mais Vendidos</Titulo>
-            <Fileira>
-                <Card imagem="/azulDeitado.png" avaliacoes={4} nomeLivro="Livro 1" nomeAutor="Autor 1" precoLivro="R$999.99" />
-                <Card imagem="/azulDeitado.png" avaliacoes={4} nomeLivro="Livro 2" nomeAutor="Autor 2" precoLivro="R$999.99" />
-                <Card imagem="/azulDeitado.png" avaliacoes={5} nomeLivro="Livro 3" nomeAutor="Autor 3" precoLivro="R$999.99" />
-                <Card imagem="/azulDeitado.png" avaliacoes={5} nomeLivro="Livro 4" nomeAutor="Autor 4" precoLivro="R$999.99" />
-            </Fileira>
-            <Fileira>
-                <Card imagem="/azulDeitado.png" avaliacoes={4} nomeLivro="Livro 5" nomeAutor="Autor 5" precoLivro="R$999.99" />
-                <Card imagem="/azulDeitado.png" avaliacoes={4} nomeLivro="Livro 6" nomeAutor="Autor 6" precoLivro="R$999.99" />
-                <Card imagem="/azulDeitado.png" avaliacoes={5} nomeLivro="Livro 7" nomeAutor="Autor 7" precoLivro="R$999.99" />
-                <Card imagem="/azulDeitado.png" avaliacoes={5} nomeLivro="Livro 8" nomeAutor="Autor 8" precoLivro="R$999.99" />
-            </Fileira>
-        </Container>
+      <Container>
+        <Titulo>Mais Vendidos</Titulo>
+        <Fileira>
+            {books.map((book) => (
+              <Card
+                key={book.id}
+                imagem="/azulDeitado.png"
+                nome={book.Nome}
+                preco={book.Preco}
+                avaliacoes={4.2} // Supondo que a avaliação seja 4.2
+              />
+            ))}
+        </Fileira>
+      </Container>
     );
-}
+  }
