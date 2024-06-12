170b8c3b5e6cb43cad2bd21036d8396755a8771b
Juan <juanpfreire18@gmail.com>
2024-06-12 17:23:38 -0300

    Estilizacao completa do allproductpage

---
+++ src/routes/AllProductsPage/AllProductPageStyle.jsx
@@ -0,0 +1,229 @@
+import { styled } from "styled-components";
+
+// Container principal
+export const Container = styled.div`
+  display: flex;
+  text-align: center;
+  margin: 24px 60px 20px 200px;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+`;
+
+// Menu colapsável
+export const MenuContainer = styled.div`
+  padding: 16px;
+  transition: transform 0.3s ease-in-out;
+
+  &.expanded .MenuContent {
+    transform: translateY(0);
+  }
+
+  &.collapsed .MenuContent {
+    transform: translateY(-100%);
+  }
+`;
+
+export const MenuItem = styled.div`
+  cursor: pointer;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 12px 0;
+  border-bottom: 1.2px solid #6d6d6d6c;
+`;
+
+export const MenuTitle = styled.h3`
+  margin: 0; 
+  font-size: 1.2rem; 
+  font-weight: 500;
+  padding: 1rem 4.4rem 0.4rem 0; 
+`;
+
+export const MenuContent = styled.div`
+  display: ${({ expanded }) => (expanded ? "block" : "none")};
+`;
+
+// Seção de Filtros
+export const FilterSection = styled.section`
+  display: grid;
+  margin: 20px 0;
+  width: 200px;
+`;
+
+export const RadioContainer = styled.div`
+  display: grid;
+  margin: 20px 0;
+  gap: 10px;
+  grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+`;
+
+export const RadioButtonLabel = styled.label`
+  display: flex;
+  margin: 0px;
+  font-weight: 400; 
+  color: #000;
+  list-style: outside none none;
+
+  input[type="radio"] {
+    margin-right: 8px;
+    background-color: #f0f0f0;
+    transition: 0.2s all linear;
+    border: none;
+    cursor: pointer;
+    width: 1.2rem;
+    height: auto;
+    vertical-align: middle;
+
+    &:checked {
+      background-color: #0e0e0e;
+      border: 1.2px dashed #020200;
+    }
+  }
+`;
+
+// Lista de Produtos
+export const ProductList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  background-color: #f8f4f4;
+  
+`;
+
+// Card do Produto
+export const ProductCard = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  margin: 10px;
+  margin-left: 2vw;
+  padding: 20px;
+  width: 1200px; 
+  height: 260px;
+  box-shadow: 10px 10px 30px #f2f2f2, -10px -10px 30px #ffffff;
+  background: #ffffff;
+`;
+
+// Seção da Imagem
+export const ProductImageSection = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ProductImage = styled.img`
+  max-width: 50%;
+  height: auto;
+`;
+
+export const ProductDetailsSection = styled.div`
+  flex: 2;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+// Nome do produto
+  export const ProductName = styled.h1`
+    margin-bottom: 20px; 
+  `;
+
+// Sinopse
+export const ProductSynopsis = styled.p`
+  max-width: calc(80% - 1px); 
+  height: 400px;
+  overflow-wrap: break-word; 
+  margin-top: 20px; 
+  text-align: justify; 
+`;
+
+// Autor
+export const ProductAuthor = styled.span`
+  margin-top: 30px; 
+  margin-right: 1.6vw;
+`;
+
+// Editora
+export const ProductPublisher = styled.span`
+  margin-top: 30px; 
+`;
+
+
+// Seção de Preço e Botões
+export const ProductPriceAndButtonsSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start; 
+  align-items: flex-start;
+  border-left: 1.2px solid #c0c0c0;
+  position: relative; 
+  padding-left: 12px;
+`;
+
+// Seção de Autor e Editora
+export const AuthorPublisherSection = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+`;
+
+export const ProductPrice = styled.p`
+  font-weight: bold;
+  text-decoration: line-through;
+  color: #505050;
+`;
+
+export const DiscountedPrice = styled.p`
+  font-size: 22px;
+  color: #333;
+  border-radius: 5px;
+`;
+
+export const BuyButton = styled.button`
+  width: 240px;
+  height: 40px;
+  background-color: black;
+  color: white;
+  border: none;
+  cursor: pointer;
+  margin-top: auto;
+  border-radius: 24px;
+  font-weight: 600;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #333; 
+  }
+`;
+
+export const MoreInfoButton = styled.button`
+  width: 240px;
+  height: 40px;
+  background-color: white;
+  color: black;
+  border: 1px solid black;
+  cursor: pointer;
+  margin-top: 10px;
+  border-radius: 24px;
+  font-weight: 600;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #f0f0f0; 
+  }
+`;
+
+// Mensagem de Erro
+export const ErrorMessage = styled.div`
+  color: #000000;
+  background-color: #ffe6e6;
+  border: 1px solid red;
+  padding: 10px;
+  margin: 10px 0; 
+  border-radius: 5px;
+  font-weight: bold;
+`;
--- src/routes/AllProductsPage/AllProductsPage.jsx
+++ src/routes/AllProductsPage/AllProductsPage.jsx
@@ -1,124 +1,258 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import styled from 'styled-components';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  AuthorPublisherSection,
+  BuyButton,
+  Container,
+  DiscountedPrice,
+  ErrorMessage,
+  FilterSection,
+  MenuContainer,
+  MenuContent,
+  MenuItem,
+  MenuTitle,
+  MoreInfoButton,
+  ProductAuthor,
+  ProductCard,
+  ProductDetailsSection,
+  ProductImage,
+  ProductImageSection,
+  ProductList,
+  ProductName,
+  ProductPrice,
+  ProductPriceAndButtonsSection,
+  ProductPublisher,
+  ProductSynopsis,
+  RadioButtonLabel,
+  RadioContainer,
+} from "./AllProductPageStyle.jsx";
 
-// Styled components
-const Container = styled.div`
-  text-align: center;
-`;
-
-const FilterSection = styled.section`
-  margin: 20px 0;
-`;
-
-const RadioButtonLabel = styled.label`
-  margin: 0 10px;
-`;
-
-const ProductList = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-`;
-
-const ProductCard = styled.div`
-  border: 1px solid #ddd;
-  margin: 10px;
-  padding: 20px;
-  width: 300px;
-`;
-
-const ProductImage = styled.img`
-  max-width: 100%;
-  height: auto;
-`;
-
-const ProductName = styled.h2`
-  color: #333;
-`;
-
-const ProductSynopsis = styled.p`
-  color: #666;
-`;
-
-const ProductPrice = styled.p`
-  font-weight: bold;
-`;
-
-const ProductAuthor = styled.p`
-  color: #0077cc;
-`;
-
-const ProductPublisher = styled.p`
-  color: #ff6600;
-`;
-
-const ErrorMessage = styled.div`
-  color: red;
-`;
-
-// Component
 const ProductPage = () => {
   const [produtos, setProdutos] = useState([]);
   const [erro, setErro] = useState(null);
-  const [filtroGenero, setFiltroGenero] = useState('');
-  const [filtroEditora, setFiltroEditora] = useState('');
+  const [filtroGenero, setFiltroGenero] = useState("");
+  const [filtroEditora, setFiltroEditora] = useState("");
+  const [filtroPreco, setFiltroPreco] = useState("");
+  const [expanded, setExpanded] = useState(true);
+  const [expandedGenre, setExpandedGenre] = useState(false);
+  const [expandedPublisher, setExpandedPublisher] = useState(false);
+  const [expandedPrice, setExpandedPrice] = useState(false);
+
+  const toggleGenreMenu = () => {
+    setExpandedGenre(!expandedGenre);
+  };
+
+  const togglePublisherMenu = () => {
+    setExpandedPublisher(!expandedPublisher);
+  };
+
+  const togglePriceMenu = () => {
+    setExpandedPrice(!expandedPrice);
+  };
+
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
 
   useEffect(() => {
-    axios.get('https://node-routes-mysql.vercel.app/book/')
-      .then(response => {
+    axios
+      .get("https://node-routes-mysql.vercel.app/book/")
+      .then((response) => {
         let produtosFiltrados = response.data;
         if (filtroGenero) {
-          produtosFiltrados = produtosFiltrados.filter(produto => produto.genre.name === filtroGenero);
+          produtosFiltrados = produtosFiltrados.filter(
+            (produto) => produto.genre.name === filtroGenero
+          );
         }
         if (filtroEditora) {
-          produtosFiltrados = produtosFiltrados.filter(produto => produto.publishing_company.name === filtroEditora);
+          produtosFiltrados = produtosFiltrados.filter(
+            (produto) => produto.publishing_company.name === filtroEditora
+          );
+        }
+        if (filtroPreco) {
+          produtosFiltrados = produtosFiltrados.filter((produto) => {
+            switch (filtroPreco) {
+              case "10":
+                return produto.price <= 10;
+              case "50":
+                return produto.price > 10 && produto.price <= 50;
+              case "100":
+                return produto.price > 50 && produto.price <= 100;
+              default:
+                return produto.price > 100;
+            }
+          });
         }
         setProdutos(produtosFiltrados);
       })
-      .catch(error => {
-        setErro('Erro ao buscar produtos');
+      .catch((error) => {
+        setErro("Erro ao buscar produtos");
       });
-  }, [filtroGenero, filtroEditora]);
-
-  if (erro) {
-    return <ErrorMessage>{erro}</ErrorMessage>;
-  }
+  }, [filtroGenero, filtroEditora, filtroPreco]);
 
   return (
     <Container>
-      <FilterSection>
-        <RadioButtonLabel>
-          <input type="radio" value="Suspense" name="genero" onChange={(e) => setFiltroGenero(e.target.value)} /> Suspense
-        </RadioButtonLabel>
-        <RadioButtonLabel>
-          <input type="radio" value="Terror" name="genero" onChange={(e) => setFiltroGenero(e.target.value)} /> Terror
-        </RadioButtonLabel>
-        <RadioButtonLabel>
-          <input type="radio" value="rocco" name="editora" onChange={(e) => setFiltroEditora(e.target.value)} /> Rocco
-        </RadioButtonLabel>
-        <RadioButtonLabel>
-          <input type="radio" value="Wish" name="editora" onChange={(e) => setFiltroEditora(e.target.value)} /> Wish
-        </RadioButtonLabel>
-        <RadioButtonLabel>
-          <input type="radio" value="CDL" name="editora" onChange={(e) => setFiltroEditora(e.target.value)} /> CDL
-        </RadioButtonLabel>
-      </FilterSection>
+      <MenuContainer style={{ minHeight: "700px" }}>
+        <MenuContent expanded={expanded}>
+          <FilterSection>
+            <MenuItem onClick={toggleGenreMenu}>
+              <MenuTitle>Gênero</MenuTitle>
+              <span>{expandedGenre ? "-" : "+"}</span>
+            </MenuItem>
+            {expandedGenre && (
+              <RadioContainer>
+                <RadioButtonLabel>
+                  <input
+                    type="radio"
+                    value="Suspense"
+                    name="genero"
+                    onChange={(e) => setFiltroGenero(e.target.value)}
+                  />{" "}
+                  Suspense
+                </RadioButtonLabel>
+                <RadioButtonLabel>
+                  <input
+                    type="radio"
+                    value="Terror"
+                    name="genero"
+                    onChange={(e) => setFiltroGenero(e.target.value)}
+                  />{" "}
+                  Terror
+                </RadioButtonLabel>
+              </RadioContainer>
+            )}
+            <MenuItem onClick={togglePublisherMenu}>
+              <MenuTitle>Editora</MenuTitle>
+              <span>{expandedPublisher ? "-" : "+"}</span>
+            </MenuItem>
+            {expandedPublisher && (
+              <RadioContainer>
+                <RadioButtonLabel>
+                  <input
+                    type="radio"
+                    value="rocco"
+                    name="editora"
+                    onChange={(e) => setFiltroEditora(e.target.value)}
+                  />{" "}
+                  Rocco
+                </RadioButtonLabel>
+                <RadioButtonLabel>
+                  <input
+                    type="radio"
+                    value="Wish"
+                    name="editora"
+                    onChange={(e) => setFiltroEditora(e.target.value)}
+                  />{" "}
+                  Wish
+                </RadioButtonLabel>
+                <RadioButtonLabel>
+                  <input
+                    type="radio"
+                    value="CDL"
+                    name="editora"
+                    onChange={(e) => setFiltroEditora(e.target.value)}
+                  />{" "}
+                  CDL
+                </RadioButtonLabel>
+              </RadioContainer>
+            )}
+            <MenuItem onClick={togglePriceMenu}>
+              <MenuTitle>Preço</MenuTitle>
+              <span>{expandedPrice ? "-" : "+"}</span>
+            </MenuItem>
+            {expandedPrice && (
+              <RadioContainer>
+                <RadioButtonLabel>
+                  <input
+                    type="radio"
+                    value=""
+                    name="preco"
+                    onChange={(e) => setFiltroPreco(e.target.value)}
+                  />{" "}
+                  Qualquer
+                </RadioButtonLabel>
+                <RadioButtonLabel>
+                  <input
+                    type="radio"
+                    value="10"
+                    name="preco"
+                    onChange={(e) => setFiltroPreco(e.target.value)}
+                  />{" "}
+                  Até R$10
+                </RadioButtonLabel>
+                <RadioButtonLabel>
+                  <input
+                    type="radio"
+                    value="50"
+                    name="preco"
+                    onChange={(e) => setFiltroPreco(e.target.value)}
+                  />{" "}
+                  R$10 até R$50
+                </RadioButtonLabel>
+                <RadioButtonLabel>
+                  <input
+                    type="radio"
+                    value="100"
+                    name="preco"
+                    onChange={(e) => setFiltroPreco(e.target.value)}
+                  />{" "}
+                  R$50 até R$100
+                </RadioButtonLabel>
+                <RadioButtonLabel>
+                  <input
+                    type="radio"
+                    value="mais"
+                    name="preco"
+                    onChange={(e) => setFiltroPreco(e.target.value)}
+                  />{" "}
+                  Mais que R$100
+                </RadioButtonLabel>
+              </RadioContainer>
+            )}
+          </FilterSection>
+        </MenuContent>
+      </MenuContainer>
 
       <ProductList>
         {produtos.length > 0 ? (
           produtos.map((produto) => (
             <ProductCard key={produto.id}>
-              <ProductName>{produto.name}</ProductName>
-              <ProductImage src={produto.image.url} alt={`Capa do livro ${produto.name}`} />
-              <ProductSynopsis>{produto.synopsis}</ProductSynopsis>
-              <ProductPrice>Preço: R$ {produto.price}</ProductPrice>
-              <ProductAuthor>Autor: {produto.author.name}</ProductAuthor>
-              <ProductPublisher>Editora: {produto.publishing_company.name}</ProductPublisher>
+              <ProductImageSection>
+                <ProductImage
+                  src={produto.image.url}
+                  alt={`Capa do livro ${produto.name}`}
+                />
+              </ProductImageSection>
+
+              <ProductDetailsSection>
+                <ProductName>{produto.name}</ProductName>
+                <ProductSynopsis>{produto.synopsis}</ProductSynopsis>
+                <AuthorPublisherSection>
+                  <ProductAuthor>Autor: {produto.author.name}</ProductAuthor>
+                  <ProductPublisher>
+                    Editora:{" "}
+                    {capitalizeFirstLetter(produto.publishing_company.name)}
+                  </ProductPublisher>
+                </AuthorPublisherSection>
+              </ProductDetailsSection>
+
+              <ProductPriceAndButtonsSection>
+                <ProductPrice>R$ {produto.price}</ProductPrice>
+                <DiscountedPrice>
+                  R$ {produto.price - (produto.price * 10) / 100}{" "}
+                  <span style={{ color: "#b8b8b8", fontSize: ".80vw" }}>
+                    à vista
+                  </span>{" "}
+                </DiscountedPrice>
+                <BuyButton>Comprar agora</BuyButton>
+                <MoreInfoButton>Saiba mais</MoreInfoButton>
+              </ProductPriceAndButtonsSection>
             </ProductCard>
           ))
         ) : (
-          <ErrorMessage>Nenhum produto encontrado para os filtros aplicados.</ErrorMessage>
+          <ErrorMessage>
+            Nenhum produto encontrado para os filtros aplicados.
+          </ErrorMessage>
         )}
       </ProductList>
     </Container>
@@ -126,5 +260,3 @@ const ProductPage = () => {
 };
 
 export default ProductPage;
-
-
